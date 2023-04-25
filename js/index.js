1e13fca0d3f37ec08a6868acf279b816461f68dc
Thiago Frazzon Arend <thiago.arend@gmail.com>
2023-04-25 20:18:25 -0300

    Inserindo comentários para manutenção do código

--- js/index.js
+++ js/index.js
@@ -2,37 +2,37 @@
 axios.defaults.headers.common['Authorization'] = 'jJo7ORw9ajCoGsHMsNZCQBo7';
 
 // variáveis globais
-let userName;
-const link = document.querySelector("input");
+let userName;                                        // nome usuário
+const link = document.querySelector("input");        // input
 
 // monitora teclado para atualizar layout do botao
 link.addEventListener(("keyup"), habilitaBotao);
 
 // funções
 function habilitaBotao() {
-    const botao = document.querySelector("button");
+    const botao = document.querySelector("button");   // button
 
     // validação da montagem da roupa
-    const modelo = document.querySelector(".modelo");
-    const gola = document.querySelector(".gola");
-    const tecido = document.querySelector(".tecido");
+    const modelo = document.querySelector(".modelo"); // div modelo
+    const gola = document.querySelector(".gola");     // div gola
+    const tecido = document.querySelector(".tecido"); // div tecido
 
     // validação input
-    const regex = /^(?:https?:\/\/)?(w{3}\.)?[\w_-]+((\.\w{2,}){1,2})(\/([\w\._-]+\/?)*(\?[\w_-]+=[^\?\/&]*(\&[\w_-]+=[^\?\/&]*)*)?)?$/gm;
-    const passouRegex = link.value.match(regex) !== null; // flag guarda true se regex encontrou pelo menos um match
+    const regexValidaInput = /^(?:https?:\/\/)?(w{3}\.)?[\w_-]+((\.\w{2,}){1,2})(\/([\w\._-]+\/?)*(\?[\w_-]+=[^\?\/&]*(\&[\w_-]+=[^\?\/&]*)*)?)?$/gm; // regex para validação da url do input
+    const passouRegex = link.value.match(regexValidaInput) !== null; // flag guarda true se regex encontrou pelo menos um match
 
     // querySelector devolve null caso nao exista o nodo procurado (null é avaliado como false)
-    if (modelo.querySelector(".selecionado")
-        && gola.querySelector(".selecionado") 
-        && tecido.querySelector(".selecionado")
-        && passouRegex) 
+    if (modelo.querySelector(".selecionado")    // se foi selecionado item em modelo && 
+        && gola.querySelector(".selecionado")   // em gola && 
+        && tecido.querySelector(".selecionado") // em tecido &&
+        && passouRegex)                         // a flag de validação do input for verdadeira
     {
-        botao.classList.add("validado");
+        botao.classList.add("validado");        // altera layout do botao e habilita
         botao.disabled = false;
     }
-    else
+    else                                        // senão
     {
-        botao.classList.remove("validado");
+        botao.classList.remove("validado");     // altera layout do botao e desabilita
         botao.disabled = true;
     }
 }
@@ -40,10 +40,10 @@ function habilitaBotao() {
 function efeitosMontarBlusa(elemento){
     // querySelector devolve null caso nao exista o nodo procurado (null é avaliado como false)
     const selecionado = elemento.querySelector(".selecionado");
-    if (selecionado) { // clicou em um item já selecionado
-        selecionado.classList.toggle("selecionado");
+    if (selecionado) { // se clicou em um item já selecionado
+        selecionado.classList.toggle("selecionado"); // altera seu estilo
     }
-    else { // clicou em um item ainda não selecionado
+    else { // senão, se clicou em um item ainda não selecionado
         // seleciona categoria
         const categoria = elemento.parentNode;
         // seleciona os círculos da categoria
